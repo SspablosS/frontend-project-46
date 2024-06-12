baab0d876bcd04e3e1ca6f71f4763b78448da7f3
SspablosS <pavelmeshkov2004@gmail.com>
2024-06-13 00:23:41 +0300

    filepath fix

--- bin/cli.js
+++ bin/cli.js
@@ -3,10 +3,13 @@
 /* eslint-disable no-console */
 
 import { Command } from 'commander';
+import path from 'path';
 import genDiff from '../src/gendiff.js';
 import parseFile from '../src/parsers.js';
 
 const program = new Command();
+
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
 
 program
   .version('1.0.0')
@@ -14,8 +17,8 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    const data1 = parseFile(filepath1);
-    const data2 = parseFile(filepath2);
+    const data1 = parseFile(getFixturePath(filepath1));
+    const data2 = parseFile(getFixturePath(filepath2));
     const diff = genDiff(data1, data2);
     console.log(diff);
   })
